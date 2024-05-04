68df949b3c7796fa5aa213d76067a13c9d10c563
pabdu <abdullalipeyman@gmail.com>
2024-05-04 17:14:50 +0400

    edit services

--- src/main/java/com/example/msproductcatalog/service/OrderService.java
+++ src/main/java/com/example/msproductcatalog/service/OrderService.java
@@ -1,9 +1,30 @@
 package com.example.msproductcatalog.service;
 
+import com.example.msproductcatalog.dao.entity.BasketEntity;
+import com.example.msproductcatalog.dao.entity.OrderDetailsEntity;
+import com.example.msproductcatalog.dao.repository.BasketRepository;
+import com.example.msproductcatalog.mapper.OrderDetailsMapper;
+import com.example.msproductcatalog.mapper.OrderMapper;
+import com.example.msproductcatalog.model.request.OrderDetailsRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
+    private final BasketRepository basketRepository;
+    private final ProductService productService;
+
+    public void addOrder(OrderDetailsRequest detailsRequest) {
+        BasketEntity basketEntity = basketRepository.findById(detailsRequest.getBasketId()).orElseThrow();
+//        basketEntity.setProducts(basketEntity.getProducts().
+//                stream().
+//                peek(product -> product.setProductCount(product.getProductCount() - 1)).
+//                toList());
+        productService.editProductsForCount(basketEntity.getProducts());
+        OrderDetailsEntity orderDetailsEntity = OrderDetailsMapper.INSTANCE.mapRequestToEntity(detailsRequest, basketEntity);
+        OrderMapper.INSTANCE.mapRequestToEntity(basketEntity, orderDetailsEntity, true);
+
+        basketRepository.save(basketEntity);
+    }
 }
--- src/main/java/com/example/msproductcatalog/service/ProductService.java
+++ src/main/java/com/example/msproductcatalog/service/ProductService.java
@@ -29,13 +29,26 @@ public class ProductService {
         return ProductMapper.INSTANCE.mapEntityListToResponseList(all);
     }
 
+    public ProductResponse getProductById(long id) {
+        ProductEntity productEntity = productRepository.findById(id).orElseThrow();
+        return ProductMapper.INSTANCE.entityToResponse(productEntity);
+    }
+
+    public void deleteProduct(long id) {
+        productRepository.deleteById(id);
+    }
+
+    public void editProductsForCount(List<ProductEntity> entities) {
+         entities.forEach(i -> i.setProductCount(i.getProductCount() - 1));
+    }
+
     public void editProduct(long id, ProductRequest productRequest) {
         ProductEntity productEntity = productRepository.findById(id).orElseThrow();
         CategoryEntity categoryEntity = null;
         if (productRequest.getCategoryId() != 0) {
             categoryEntity = categoryRepository.findById(productRequest.getCategoryId()).orElseThrow();
         }
-        ProductMapper.INSTANCE.editProduct(productEntity, categoryEntity, id, productRequest);
+        ProductMapper.INSTANCE.mapRequestToEntity(productEntity, categoryEntity, id, productRequest);
         productRepository.save(productEntity);
     }
 }
