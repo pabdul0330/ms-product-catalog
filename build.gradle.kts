21959d9995b398b1602f66dffb6131c5a3e5f7a2
pabdu <abdullalipeyman@gmail.com>
2024-05-20 22:35:55 +0400

    Fix dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ extra["springCloudVersion"] = "2023.0.1"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
 
+
     //lombok
     annotationProcessor("org.projectlombok:lombok")
     compileOnly("org.projectlombok:lombok")
@@ -29,24 +30,34 @@ dependencies {
     //feign client
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.1.0")
 
-    //security
-    implementation("org.springframework.boot:spring-boot-starter-security")
-
     //database
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     //implementation("org.liquibase:liquibase-core")
     runtimeOnly("org.postgresql:postgresql")
+
+    //validation
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+
     //security
-    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-security:3.2.4")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf:3.2.4")
+
+
     //swagger
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+    implementation("io.swagger:swagger-annotations:1.6.4")
+
     //spock
     testImplementation("org.spockframework:spock-core:2.4-M1-groovy-4.0")
+
     //random
     testImplementation("io.github.benas:random-beans:3.9.0")
+
     //mapstruck
     implementation("org.mapstruct:mapstruct:1.4.2.Final")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.4.2.Final")
+
     //test
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
